c841f679c96dab80d41c2ee6596746d86721f64f
elbrianmcdonald-dev <52047322+elbrianmcdonald-dev@users.noreply.github.com>
2019-06-25 12:35:37 -0500

    Add files via upload

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 docker rmi harvarditsecurity/misp
 docker build \
     --rm=true --force-rm=true \
-    --build-arg MYSQL_MISP_PASSWORD=ChangeThisDefaultPassworda9564ebc3289b7a14551baf8ad5ec60a \
+    --build-arg MYSQL_MISP_PASSWORD=Einsof1414 \
     --build-arg POSTFIX_RELAY_HOST=localhost \
-    --build-arg MISP_FQDN=localhost \
-    --build-arg MISP_EMAIL=admin@localhost \
-    --build-arg MISP_GPG_PASSWORD=ChangeThisDefaultPasswordXuJBao5Q2bps89LWFqWkKgDZwAFpNHvc \
+    --build-arg MISP_FQDN=DALPUBUNTU01 \
+    --build-arg MISP_EMAIL=brian.mcdonald@mcdermott.com \
+    --build-arg MISP_GPG_PASSWORD=Einsof1414 \
     -t harvarditsecurity/misp container
